b8f91b5a48160cb862fb2bd811d6f599c70be693
Givikap120 <givikap120@gmail.com>
2025-12-10 21:06:42 +0100

    Add additional function to CSV exporter

--- PerformanceCalculatorGUI/Utils/CSVExporter.cs
+++ PerformanceCalculatorGUI/Utils/CSVExporter.cs
@@ -4,7 +4,6 @@ using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading;
-using PerformanceCalculatorGUI.Screens;
 
 namespace PerformanceCalculatorGUI.Utils
 {
@@ -25,6 +24,29 @@ namespace PerformanceCalculatorGUI.Utils
             foreach (var line in info)
             {
                 csvContent.AppendLine(line.GetCSV());
+            }
+
+            File.WriteAllText(filepath, csvContent.ToString());
+
+            Thread.CurrentThread.CurrentCulture = currentCulture;
+            Console.WriteLine($"CSV file created at: {filepath}");
+        }
+
+        public static void ExportToCSV(IEnumerable<string> info, string header, string filepath)
+        {
+            // Ensure Dot as a separator
+            System.Globalization.CultureInfo customCulture = (System.Globalization.CultureInfo)Thread.CurrentThread.CurrentCulture.Clone();
+            customCulture.NumberFormat.NumberDecimalSeparator = ".";
+
+            var currentCulture = Thread.CurrentThread.CurrentCulture;
+            Thread.CurrentThread.CurrentCulture = customCulture;
+
+            var csvContent = new StringBuilder();
+            if (header != "") csvContent.AppendLine(header);
+
+            foreach (string line in info)
+            {
+                csvContent.AppendLine(line);
             }
 
             File.WriteAllText(filepath, csvContent.ToString());
